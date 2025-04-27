44696086c902b1a9dee931ec3794e20f6e5f98cd
zuher <zuherash@gmail.com>
2025-04-28 00:01:22 +0300

    Feat: Book details

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.pagination import PageNumberPagination
+
+class Bookpagination(PageNumberPagination):
+    page_size='5'
+
+class BookRetrieveAPIView(generics.RetrieveAPIView):
+    queryset=Book.objects.all()
+    serializer_class= BookSerializer
+    lookup_field='id'
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = Bookpagination
