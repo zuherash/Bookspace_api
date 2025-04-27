1410d6f290075947b69b72b71bbffc419809fbe4
zuher <zuherash@gmail.com>
2025-04-28 00:35:16 +0300

    Feat: delete book api

--- core/views.py
+++ core/views.py
@@ -20,3 +20,8 @@ class BookUpdateAPIView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
+
+class BookDeleteAPIView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
