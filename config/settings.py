b9afac10fdfc550a12d60a0b1ad49c3a3c6979ed
zuher <zuherash@gmail.com>
2025-04-28 18:44:47 +0300

    AUTH

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'django_filters',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ]
+}
--- core/filters.py
+++ core/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import Book
 
-class BookFilter(django_filters.filterset):
+class BookFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
     author = django_filters.CharFilter(lookup_expr='icontains')
     available = django_filters.BooleanFilter()
